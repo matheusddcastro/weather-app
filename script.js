4b3afcca75bc6e6e8c0daf01411eccf3fe794bb7
matheusddcastro <matheusdouradocastro@gmail.com>
2025-07-23 11:34:28 +0100

    Added addWeatherDashboard() function

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const form = document.getElementById("weather-form");
 const cityInput = document.getElementById("city-input");
 const weatherResult = document.getElementById("weather-result");
+
+// Adding Weather Dashboard to the DOM
+function addWeatherDashboard() {
+    const weatherDashboard = document.createElement("div");
+    weatherDashboard.className = "weather-dashboard";
+    weatherResult.appendChild(weatherDashboard);
+
+}
