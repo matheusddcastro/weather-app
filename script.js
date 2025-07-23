33078c0c5fed93f153cf3b6241aeb409296fe7ee
matheusddcastro <matheusdouradocastro@gmail.com>
2025-07-23 13:14:16 +0100

    Restart - functions and fetch not working as intended

--- script.js
+++ script.js
@@ -8,42 +8,9 @@ const weatherResult = document.getElementById("weather-result")
 
 // Adding Weather Dashboard to the DOM
 function addWeatherDashboard(info) {
-  const placeName = info.name
-  const temp = info.main.temp
-  const humiditi = info.main.humidity
-  const windSpeed = info.wind.speed 
-  const weather = info.weather 
-
-  const weatherCard = document.createElement("div")
-  weatherCard.classList.add("weather-card")
-  weatherCard.innerHTML = `
-    <h2>${placeName}</h2>
-    <p>Temperature: ${temp}°C</p>
-    <p>Humidity: ${humiditi}%</p>
-    <p>Wind Speed: ${windSpeed} m/s</p>
-    <p>Weather: ${weather[0].main}</p>
-  `
-
-  addWeatherImage(weather[0].main)
-
-  weatherResult.appendChild(weatherCard)
+  
 }
 
 // Add Weather Image based on Weather Condition
-function addWeatherImage(condition) {
-  const weatherImage = document.createElement("img")
-  weatherImage.src = `./images/${condition}.png`
-  weatherResult.appendChild(weatherImage)
-}
 
 // Fetch Weather Data
-form.addEventListener("submit", async (e) => {
-    e.preventDefault()
-
-    try {
-        const response = await fetch(apiURL + cityInput.value + `&appid=${apiKey}`)
-        const data = await response.json()
-    } catch (error) {
-        console.log('Error:', error)
-    }
-})
