3529b87561dc7ccdc6e76deb006aae2d8adca907
matheusddcastro <matheusdouradocastro@gmail.com>
2025-07-23 11:06:15 +0100

    Got DOM elements

---
+++ script.js
@@ -0,0 +1,7 @@
+const apiKey = "7d8304cd57141ecb6a77fa5a1920d246";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+// Getting DOM elements
+const form = document.getElementById("weather-form");
+const cityInput = document.getElementById("city-input");
+const weatherResult = document.getElementById("weather-result");
